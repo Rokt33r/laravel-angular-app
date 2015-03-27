f4e40b42797ce4c04068576af333f03e577c8618
Dick Choi <fluke8259@gmail.com>
2015-03-27 13:03:24 +0900

    some unnecessary packages are removed.

--- build.conf.js
+++ build.conf.js
@@ -11,11 +11,8 @@ module.exports = {
 
     // vendor path
     vendor_path:[
-        'bower_components/codemirror/lib/codemirror.js',
-        'bower_components/codemirror/lib/codemirror.css',
         'bower_components/angular/angular.js',
         'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
-        'bower_components/angular-ui-codemirror/ui-codemirror.js',
         'bower_components/angular-ui-router/release/angular-ui-router.js',
         'bower_components/fontawesome/**/fonts/*', 'bower_components/fontawesome/**/css/font-awesome.css',
         'bower_components/angular-jwt/dist/angular-jwt.js'
